665b481eb1f2ee9e532ab5f6ef3996ac7e653b42
levietducanh99 <ducanhvahung@gmail.com>
2025-03-17 00:41:51 +0700

    fix confict

--- src/search_methods/full_text.py
+++ src/search_methods/full_text.py
@@ -9,7 +9,7 @@ def search(query: str):
     supabase = get_supabase_client()
 
     # Đo thời gian bắt đầu
-    start_time = time.time()
+    start_time = time.time() hellllllll
 
     # Lấy tất cả dữ liệu từ bảng WebScrapData
     response = supabase.table("WebScrapData").select("name").execute()

b1b8ef9ca475590189e05e0d39a7b3e085e51902
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-04-26 17:32:00 +0700

    Update: Change how the pipeline makes requests to Supabase, preventing rate-limit

--- src/search_methods/keyword_creation_pipeline.py
+++ src/search_methods/keyword_creation_pipeline.py
@@ -24,15 +24,19 @@ def get_synonyms(keywords):
     for keyword in keywords:
         for syn in wordnet.synsets(keyword):
             for lemma in syn.lemmas():
-                synonyms.add(lemma.name())
-    
+                name = lemma.name()
+                # Keep only single words without underscores
+                if "_" not in name and " " not in name:
+                    synonyms.add(name.lower())  # Also make sure it's lowercase if you want
     return synonyms
-    
 
 #Take headlines without keywords
 def get_headlines_without_keywords():
-    # while True:
+     while True:
         responses = supabase.rpc("get_headlines_without_keywords").execute()
+        keywords_proper_nouns_update = []
+        keywords_update = []
+
         for response in responses.data:
             headline = response["headline"]
             short_description = response["short_description"]
@@ -44,20 +48,34 @@ def get_headlines_without_keywords():
             keywords = [token.lemma_.lower() for token in doc if token.pos_ != "PROPN" and not token.is_stop and not token.is_punct]
             
             #In case no keywords are found in the headline, use the short description
-            if (not keywords_proper_nouns) and (not keywords):
+            if (not keywords_proper_nouns):
                 doc = nlp(short_description)
                 keywords_proper_nouns = [token.text.lower() for token in doc if token.pos_ == "PROPN"]
-                keywords = [token.lemma_.lower() for token in doc if token.pos_ != "PROPN" and not token.is_stop and not token.is_punct]
 
-            # print("Keywords: ", keywords)
-            #Upsert proper nouns keywords to the database
-            if (keywords_proper_nouns):
-                supabase.table("WebScrapData").upsert({"id": response["id"], "keywords_proper_nouns": keywords_proper_nouns}).execute()
+            if (not keywords):
+                doc = nlp(short_description)
+                keywords = [token.lemma_.lower() for token in doc if token.pos_ != "PROPN" and not token.is_stop and not token.is_punct]
             
-            #Find synonyms for the keywords and upsert them to the database
+            keywords_proper_nouns_update.append({
+                "id": response["id"],
+                "keywords_proper_nouns": keywords_proper_nouns
+            })
+
             keywords_and_synonyms = get_synonyms(keywords)
-            # print("Keywords and synonyms: ", keywords_and_synonyms)
-            supabase.table("WebScrapData").upsert({"id": response["id"], "keywords": list(keywords_and_synonyms)}).execute()
+            keywords_update.append({
+                "id": response["id"],
+                "keywords": list(keywords_and_synonyms)
+            })
+        
+        #Update the keywords in the database
+        if keywords_proper_nouns_update:
+            supabase.table("WebScrapData").upsert(keywords_proper_nouns_update).execute()
+        if keywords_update:
+            supabase.table("WebScrapData").upsert(keywords_update).execute()
+        
+        print("Another 1000 headlines processed, renewing connection...")
+        time.sleep(1)
+        new_supabase()
              
 if __name__ == "__main__":
     get_headlines_without_keywords()

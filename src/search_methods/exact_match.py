5c1b1750ed72d4f567cf25749620c36a3219b9fd
hoan99111 <hoan99111@gmail.com>
2025-04-28 00:52:18 +0700

    Add timing functionality and update Python version in project files

--- src/search_methods/exact_match.py
+++ src/search_methods/exact_match.py
@@ -25,11 +25,11 @@ def search(query: str):
     return response.data, execution_time
 
 
-if __name__ == "__main__":
-    # Test phương pháp Exact Match với bảng WebScrapData
-    query = "Product"
-    results, execution_time = search(query)
-    print("Exact Match Results from WebScrapData:")
-    for result in results:
-        print(f"Name: {result['name']}")
-    print(f"Thời gian thực thi: {execution_time:.4f} giây")
+# if __name__ == "__main__":
+#     # Test phương pháp Exact Match với bảng WebScrapData
+#     query = "Product"
+#     results, execution_time = search(query)
+#     print("Exact Match Results from WebScrapData:")
+#     for result in results:
+#         print(f"Name: {result['name']}")
+#     print(f"Thời gian thực thi: {execution_time:.4f} giây")
